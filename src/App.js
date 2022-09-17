b8ddeaadf7853e21942386e83b782bd7ec004024
41Uday <uday1729vv>
2022-09-17 10:55:16 +0000

    foodDeliveryApp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Component} from 'react'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default App
